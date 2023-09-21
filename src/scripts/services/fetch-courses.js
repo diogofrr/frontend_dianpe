4bd894f00e1e5cdbbe75433746509d162a5adaf3
Diogo Ferreira <ddiogof20@gmail.com>
2023-09-20 21:32:33 -0300

    página de cursos iniciada

---
+++ src/scripts/services/fetch-courses.js
@@ -0,0 +1,35 @@
+(function () {
+  const schoolsHtmlList = document.getElementById("courses__list");
+  const spinner = document.getElementById("courses-spinner");
+
+  async function fetchSchools() {
+    try {
+      const result = await fetch("https://api-dianpe.onrender.com/cursos/");
+      return result.json();
+    } catch (error) {
+      console.error("Erro ao buscar escolas:", error);
+      return [];
+    }
+  }
+
+  function schoolCardTemplate(id, schoolName, schoolImg, schoolImgAlt) {
+    return (`
+      <li class="schoolsCardSection__school">
+        <a href="school.html?id=${id}" class="schoolsCardSection__schoolLink">
+          <img class="schoolsCardSection__img" src="${schoolImg}" alt="${schoolImgAlt}" />
+          <p class="schoolsCardSection__schoolDesc">
+            <b>${schoolName}</b>
+          </p>
+        </a>
+      </li>
+    `);
+  }
+
+  async function renderSchools() {
+    const { CURSOS_POR_CATEGORIA } = await fetchSchools();
+
+    console.log(CURSOS_POR_CATEGORIA)
+  }
+
+  renderSchools();
+})();
