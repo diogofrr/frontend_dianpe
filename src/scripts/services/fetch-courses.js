d9c7d98747f9f06b14528b1a169da4a24bcf59af
Diogo Ferreira <ddiogof20@gmail.com>
2023-09-21 21:22:06 -0300

    layout da página cursos finalizado

--- src/scripts/services/fetch-courses.js
+++ src/scripts/services/fetch-courses.js
@@ -2,7 +2,7 @@
   const coursesHtmlList = document.getElementById("courses__list");
   const spinner = document.getElementById("courses-spinner");
 
-  async function fetchSchools() {
+  async function fetchCourses() {
     try {
       const result = await fetch("https://api-dianpe.onrender.com/cursos/");
       return result.json();
@@ -12,24 +12,35 @@
     }
   }
 
-  function schoolCardTemplate(id, schoolName, schoolImg, schoolImgAlt) {
+  function courseSectionTemplate(categoryName, courseList) {
     return (`
-      <li class="schoolsCardSection__school">
-        <a href="school.html?id=${id}" class="schoolsCardSection__schoolLink">
-          <img class="schoolsCardSection__img" src="${schoolImg}" alt="${schoolImgAlt}" />
-          <p class="schoolsCardSection__schoolDesc">
-            <b>${schoolName}</b>
+    <div class="typeCoursesCategory">
+      <p class="typeCoursesCategory__categoryTitle">${categoryName}</p>
+      <ul class="typeCoursesCategory__courses main-carousel" id="courses__list"  data-flickity='{ "cellAlign": "left", "contain": true }'>
+        ${courseList.map((course) => coursesCardTemplate())}
+      </ul>
+    <div>
+    `)
+  }
+
+  function coursesCardTemplate(id, courseImg, courseName, courseImgAlt) {
+    return (`
+      <li class="carousel-cell">
+        <a href="${id}" class="courses__link">
+          <img class="courses__img" src="${courseImg}" alt="${courseImgAlt}" />
+          <p class="courses__name">
+            ${courseName}
           </p>
         </a>
       </li>
     `);
   }
 
-  async function renderSchools() {
-    const { CURSOS_POR_CATEGORIA } = await fetchSchools();
+  async function renderCourses() {
+    const categorias = await fetchCourses();
 
-    console.log(CURSOS_POR_CATEGORIA)
+    console.log(categorias)
   }
 
-  renderSchools();
+  renderCourses();
 })();
