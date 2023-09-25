729b36a63807c130ac6fdb339a2ab1e6888cc3cf
diogofrr <ddiogof20@gmail.com>
2023-09-25 16:40:53 -0300

    barra de pesquisa iniciada

--- src/scripts/services/search-bar.js
+++ src/scripts/services/search-bar.js
@@ -1,3 +1,34 @@
 (function(){
-    
+  const searchBar = document.getElementById('search-bar');
+  const searchBarFilter = document.getElementById('search-bar-filter');
+  const searchBarContainer = document.getElementById('search-bar-container');
+  let timeoutId;
+
+  async function searchInAPI(inputValue, filter) {
+    try {
+      const url = `http://localhost:3000/${filter}/pesquisa?query=${inputValue}`;
+
+      if (inputValue.length >= 3 && filter) {
+        const result = await fetch(url);
+        const parsedValue = await result.json()
+        console.log(parsedValue)
+      } else {
+        console.log('Valor pequeno')
+      }
+    } catch (e) {
+      console.error('Houve um erro ao pesquisar dados')
+    }
+  }
+
+  function searchValue(event) {
+    clearTimeout(timeoutId);
+    // Inicia um novo setTimeout para exibir o valor após 1 segundo
+    timeoutId = setTimeout(() => {
+      searchInAPI(event.target.value, searchBarFilter.value)
+    }, 500); // 500 milissegundos = 0,5 segundos
+  }
+
+  searchBar.addEventListener('focusin', () => searchBarContainer.style.outline = '2px solid black')
+  searchBar.addEventListener('focusout', () => searchBarContainer.style.outline = '')
+  searchBar.addEventListener('input', searchValue)
 }())
